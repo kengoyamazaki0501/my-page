64fbf35c280e239fa231023e24dfb47335bc731e
kengoyamazaki0501 <kengo19890501@gmail.com>
2020-11-27 12:40:52 +0900

    ユーザー詳細編集機能（途中）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,17 @@ class UsersController < ApplicationController
     @users = User.includes(:user)
   end
 
+  def new
+    @user = User.new 
+  end
+
   def show
     @user = User.find(params[:id])
   end
 
-
+  def edit
+    @user = User.find(params[:id])
+  end
 
   private
   def user_params
