91793d094258887f4bc8d66c35b43e7952dd9f78
kengoyamazaki0501 <kengo19890501@gmail.com>
2020-11-27 15:22:24 +0900

    mypageモデル作成

---
+++ db/migrate/20201127055306_create_mypages.rb
@@ -0,0 +1,11 @@
+class CreateMypages < ActiveRecord::Migration[6.0]
+  def change
+    create_table :mypages do |t|
+      t.timestamps
+      t.string :title,           null: false
+      t.text :catch_copy,        null: false
+      t.text :concept,           null: false
+      t.references :user,        foreign_key: true
+    end
+  end
+end
