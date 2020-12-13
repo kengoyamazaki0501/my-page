0965c06d64496e81dafc10462d0a90bd6974bece
kengoyamazaki0501 <kengo19890501@gmail.com>
2020-12-13 22:37:01 +0900

    インスタグラムURL修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,19 @@
 module ApplicationHelper
   
-  def twitter_url(name)
+  def twitter_url(name) 
+    return if name.blank?
     link_to name, File.join('https://twitter.com', name)
   end
   def instagram_url(name)
+    return if name.blank?
     link_to name, File.join('https://www.instagram.com', name)
   end
-  def instagram_url(name)
+  def facebook_url(name)
+    return if name.blank?
     link_to name, File.join('https://www.facebook.com', name)
   end
   def youtube_url(url)
+    return if name.blank?
     link_to url,url 
   end
 
